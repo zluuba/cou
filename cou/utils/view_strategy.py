56ed5a6126e21a1b748e91a6ae554023ad56001b
zluuba <zluyba.nikitina@gmail.com>
2024-08-15 15:34:35 +0300

    chore: update create_view_strategy func, add strategy map

--- cou/utils/view_strategy.py
+++ cou/utils/view_strategy.py
@@ -1,15 +1,24 @@
 from cou.viewers.strategies.basic_info import BasicInfoViewStrategy
 from cou.viewers.strategies.list import ListViewStrategy
 from cou.viewers.strategies.statistics import StatisticsViewStrategy
+from cou.viewers.strategies.tree import TreeViewStrategy
 from cou.viewers.strategies.composite import CompositeViewStrategy
 
 
-def get_view_strategy_class(list_view=False, statistic_view=False):
+STRATEGY_MAP = {
+    'list_view': ListViewStrategy,
+    'statistic_view': StatisticsViewStrategy,
+    'tree_view': TreeViewStrategy,
+}
+
+
+def create_view_strategy(**strategy_flags):
+    """Create a view strategy based on the provided flags."""
+
     strategies = [BasicInfoViewStrategy]
 
-    if list_view:
-        strategies.append(ListViewStrategy)
-    if statistic_view:
-        strategies.append(StatisticsViewStrategy)
+    for key, strategy in STRATEGY_MAP.items():
+        if strategy_flags.get(key, False):
+            strategies.append(strategy)
 
     return CompositeViewStrategy(*strategies)
