7d2cc7566fa56effc148c0838dc8c8da9c32c144
zluuba <zluyba.nikitina@gmail.com>
2024-08-21 19:06:00 +0300

    feat: add constants file, add ErrorCollector to the entry point func

--- cou/cli/app.py
+++ cou/cli/app.py
@@ -4,6 +4,7 @@ from colorama import init as init_colors
 from cou.cli.config import Config
 from cou.core.report_collection import ReportCollection
 from cou.errors.decorators import suppress_traceback
+from cou.errors.error_collector import ErrorCollector
 from cou.viewers.report_viewer import ReportViewer
 
 
@@ -23,8 +24,11 @@ def main() -> None:
     config = Config()
     config.parse_args()
 
-    report_collection = ReportCollection(config.paths)
+    report_collection = ReportCollection(config)
     asyncio.run(report_collection.generate_all())
 
     viewer = ReportViewer(config.view_strategy)
     viewer.show(report_collection)
+
+    errors_collector = ErrorCollector(report_collection)
+    errors_collector.display_errors()
